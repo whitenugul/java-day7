74d56c339a80253ad9143e7a36a05c6cc9aa281d
whitenugul <andrewahn00@gmail.com>
2021-08-03 22:07:29 +0900

    Create Quiz2.java

---
+++ Quiz2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Quiz2 {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        System.out.print("높이는: ");
+        int height = num.nextInt();
+        // for(int i = 0; i < height; i++){
+        //     for(int j = height - i; j > 0; j--){
+        //         System.out.print("1");
+        //     }
+        //     for(int x = 0; x <i*2+1; x++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+
+
+
+    }
+}
